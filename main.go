b9ca120fa89469c46bca621bb3f545411da1e038
Parham Alvani <parham.alvani@gmail.com>
2022-12-14 09:13:59 +0330

    feat: switch to sqlite and groups

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"log"
+
 	"github.com/1995parham-teaching/students/internal/handler"
 	"github.com/1995parham-teaching/students/internal/store/student"
 	"github.com/labstack/echo/v4"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
 	app := echo.New()
 
-	s := student.NewInMemory()
+	db, err := gorm.Open(sqlite.Open("students.db"), new(gorm.Config))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := student.NewSQL(db)
 
 	h := handler.Student{
 		Store: s,
 	}
 
-	app.POST("/student", h.Create)
+	h.Register(app.Group("/v1"))
 
-	app.Start("127.0.0.1:1373")
+	if err := app.Start("127.0.0.1:1373"); err != nil {
+		log.Fatal(err)
+	}
 }
