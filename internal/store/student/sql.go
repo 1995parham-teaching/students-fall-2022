88ea6415820ecc066168e3705e76435b4a8aaa24
Parham Alvani <parham.alvani@gmail.com>
2022-12-14 09:15:05 +0330

    feat: add sql implementation student

---
+++ internal/store/student/sql.go
@@ -0,0 +1,66 @@
+package student
+
+import (
+	"log"
+
+	"github.com/1995parham-teaching/students/internal/model"
+	"gorm.io/gorm"
+)
+
+type SQLItem struct {
+	ID   string `gorm:"primaryKey"`
+	Name string
+}
+
+func (SQLItem) TableName() string {
+	return "students"
+}
+
+type SQL struct {
+	DB *gorm.DB
+}
+
+func NewSQL(db *gorm.DB) Student {
+	if err := db.AutoMigrate(new(SQLItem)); err != nil {
+		log.Fatal(err)
+	}
+
+	return SQL{
+		DB: db,
+	}
+}
+
+func (sql SQL) GetAll() ([]model.Student, error) {
+	var items []SQLItem
+
+	if err := sql.DB.Find(&items).Error; err != nil {
+		return nil, err
+	}
+
+	students := make([]model.Student, 0, len(items))
+
+	for _, item := range items {
+		students = append(students, model.Student{
+			ID:      item.ID,
+			Name:    item.Name,
+			Courses: nil,
+		})
+	}
+
+	return students, nil
+}
+
+func (sql SQL) Create(s model.Student) error {
+	if err := sql.DB.Create(&SQLItem{
+		ID:   s.ID,
+		Name: s.Name,
+	}).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sql SQL) Get(id string) (model.Student, error) {
+	return model.Student{}, nil
+}
