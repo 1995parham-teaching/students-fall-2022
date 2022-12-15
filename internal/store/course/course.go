bcae4f27648cb567d8da4409a42daa8ac4aa1028
Parham Alvani <parham.alvani@gmail.com>
2022-12-15 09:51:32 +0330

    feat: add course inferface for store

---
+++ internal/store/course/course.go
@@ -0,0 +1,18 @@
+package student
+
+import (
+	"errors"
+
+	"github.com/1995parham-teaching/students/internal/model"
+)
+
+var (
+	ErrCourseAlreadyExists = errors.New("course already exists")
+	ErrCourseNotFound      = errors.New("course does not exist")
+)
+
+type Course interface {
+	GetAll() ([]model.Course, error)
+	Create(model.Course) error
+	Get(string) (model.Course, error)
+}
