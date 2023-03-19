a8f157dc24eaf8c88191b71c85cbfce0e38062f3
Parham Alvani <parham.alvani@gmail.com>
2023-03-19 12:25:00 +0330

    fix: correct lint issue

--- internal/request/student.go
+++ internal/request/student.go
@@ -1,6 +1,8 @@
 package request
 
 import (
+	"fmt"
+
 	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
@@ -10,7 +12,11 @@ type StudentCreate struct {
 }
 
 func (r StudentCreate) Validate() error {
-	return validation.ValidateStruct(&r,
+	if err := validation.ValidateStruct(&r,
 		validation.Field(&r.Name, is.UTFLetter, validation.Length(1, 0)),
-	)
+	); err != nil {
+		return fmt.Errorf("student creation request validation failed %w", err)
+	}
+
+	return nil
 }
