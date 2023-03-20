ae939ddae246ef37399dcfa90193b21904cf6359
Parham Alvani <parham.alvani@gmail.com>
2023-03-20 19:07:24 +0330

    feat: add course request and update validation rules

--- internal/request/course.go
+++ internal/request/course.go
@@ -8,21 +8,21 @@ import (
 	"github.com/go-ozzo/ozzo-validation/v4/is"
 )
 
-type StudentCreate struct {
+type CourseCreate struct {
 	Name string `json:"name"`
 }
 
-func (r StudentCreate) Validate() error {
+func (r CourseCreate) Validate() error {
 	if err := validation.ValidateStruct(&r,
 		validation.Field(&r.Name, validation.Length(1, 0), validation.Required),
 	); err != nil {
-		return fmt.Errorf("student creation request validation failed %w", err)
+		return fmt.Errorf("course creation request validation failed %w", err)
 	}
 
 	if err := validation.Validate(strings.Fields(r.Name),
 		validation.Each(is.UTFLetter),
 	); err != nil {
-		return fmt.Errorf("student creation request validation failed %w", err)
+		return fmt.Errorf("course creation request validation failed %w", err)
 	}
 
 	return nil
