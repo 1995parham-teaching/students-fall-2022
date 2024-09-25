3ff6325505450af7ddc1d66afd08ceeca3b31176
Parham Alvani <parham.alvani@gmail.com>
2024-09-25 21:03:04 +0000

    feat: print query on error

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"log"
+	"strings"
 
 	"github.com/1995parham-teaching/students/internal/graph"
 	"github.com/1995parham-teaching/students/internal/graph/resolver"
 	"github.com/1995parham-teaching/students/internal/handler"
 	"github.com/1995parham-teaching/students/internal/store/course"
 	"github.com/1995parham-teaching/students/internal/store/student"
+	"github.com/99designs/gqlgen/graphql"
 	gHandler "github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/labstack/echo/v4"
@@ -48,6 +51,22 @@ func main() {
 
 	{
 		srv := gHandler.NewDefaultServer(graph.NewExecutableSchema(resolver.New(ss)))
+		srv.AroundResponses(func(ctx context.Context, next graphql.ResponseHandler) *graphql.Response {
+			response := next(ctx)
+
+			// HasOperationContext checks if the given context is part of an ongoing operation
+			// Some errors can happen outside of an operation, eg json unmarshal errors.
+			if graphql.HasOperationContext(ctx) {
+				oc := graphql.GetOperationContext(ctx)
+
+				if len(response.Errors) != 0 {
+					log.Println(strings.ReplaceAll(oc.RawQuery, "\n", " "))
+				}
+			}
+
+			return response
+		})
+
 		g := app.Group("/v2")
 
 		g.POST("/query", echo.WrapHandler(srv))
